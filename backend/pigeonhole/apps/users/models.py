c016d8d4e06c22497e71b07d0d1168991eb33c46
Axel Lorreyne <Axel.Lorreyne@UGent.be>
2024-02-22 22:55:32 +0100

    add account deletion

--- backend/pigeonhole/apps/users/models.py
+++ backend/pigeonhole/apps/users/models.py
@@ -4,3 +4,10 @@ from django.contrib.auth.models import AbstractUser
 class User(AbstractUser):
     class Meta(AbstractUser.Meta):
         db_table = "auth_user"
+
+    def delete_account(self):
+        self.first_name = ""
+        self.last_name = ""
+        self.email = ""
+        self.is_active = False
+        self.save()
