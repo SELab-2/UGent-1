d6ba42347d7ed4dd303f314c5a54b21d8bc48356
robinpdev <robinparet22@gmail.com>
2024-02-24 12:11:17 +0100

    try to fix static url

--- backend/pigeonhole/settings.py
+++ backend/pigeonhole/settings.py
@@ -144,7 +144,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = 'api/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
