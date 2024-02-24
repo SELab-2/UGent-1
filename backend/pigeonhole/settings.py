0e1146111796809c6633bf685f520ab9c6797b29
robinpdev <robinparet22@gmail.com>
2024-02-24 12:58:24 +0100

    try to fix static files location

--- backend/pigeonhole/settings.py
+++ backend/pigeonhole/settings.py
@@ -149,7 +149,11 @@ STATIC_ROOT = BASE_DIR / "staticfiles"
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'uploads'
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'staticfiles'),
+)
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
