82aea3049e63b5e8ebc51f22f8ae68f3c4bd7481
robinpdev <robinparet22@gmail.com>
2024-02-24 20:53:28 +0100

    moment of truth

--- backend/pigeonhole/settings.py
+++ backend/pigeonhole/settings.py
@@ -144,7 +144,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 STATIC_URL = 'static/'
-STATIC_ROOT = BASE_DIR / "staticfiles"
+STATIC_ROOT = BASE_DIR / "static"
 
 MEDIA_URL = 'media/'
 MEDIA_ROOT = BASE_DIR / 'uploads'
