37a13c92a0772f2a502f2936bdfd69f6567a8f27
robinpdev <robinparet22@gmail.com>
2024-02-24 21:04:49 +0100

    nog een experiment

--- backend/pigeonhole/settings.py
+++ backend/pigeonhole/settings.py
@@ -144,10 +144,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 STATIC_URL = 'static/'
-STATIC_ROOT = BASE_DIR / "static"
+STATIC_ROOT = BASE_DIR / "uploads"
 
 MEDIA_URL = 'media/'
-MEDIA_ROOT = BASE_DIR / 'uploads'
+MEDIA_ROOT = BASE_DIR / 'static'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
