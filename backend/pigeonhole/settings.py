b08ad634591e1a1abe8fb1250cd7de9265adc171
robinpdev <robinparet22@gmail.com>
2024-02-24 20:42:09 +0100

    remove static for now

--- backend/pigeonhole/settings.py
+++ backend/pigeonhole/settings.py
@@ -146,10 +146,8 @@ USE_TZ = True
 STATIC_URL = 'static/'
 STATIC_ROOT = BASE_DIR / "staticfiles"
 
-MEDIA_URL = '/media/'
+MEDIA_URL = 'media/'
 MEDIA_ROOT = BASE_DIR / 'uploads'
-
-STATIC_URL = '/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
