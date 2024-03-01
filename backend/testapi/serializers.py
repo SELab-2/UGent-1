140c726c591bb0287340139f2b02ed7f3bc4a613
Axel Lorreyne <axellorreyne2@gmail.com>
2024-03-01 18:25:59 +0100

    Use abstract user instead of Person

--- backend/testapi/serializers.py
+++ backend/testapi/serializers.py
@@ -1,5 +1,8 @@
-from django.contrib.auth.models import Group, User
+from django.contrib.auth import get_user_model
+from django.contrib.auth.models import Group
 from rest_framework import serializers
+
+User = get_user_model()
 
 
 class UserSerializer(serializers.HyperlinkedModelSerializer):
