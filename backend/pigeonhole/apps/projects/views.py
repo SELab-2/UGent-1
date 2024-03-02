59393c70e98e0400bc41543bb9c05eed4e8b8d92
avoyen <alexander.vanoyen@ugent.be>
2024-03-02 20:47:38 +0100

    First implementation of a view. Much work to do.

---
+++ backend/pigeonhole/apps/projects/views.py
@@ -0,0 +1,35 @@
+from django.http import JsonResponse
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.views import APIView
+
+from models import Project
+
+
+class ProjectAPIView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request, subject_id, project_id):
+        # TODO still figuring out how to implement views properly.
+        if request.user.teacher:
+            # TODO should be able to get every project as an admin, not as a teacher. (check the is_admin field)
+            # This happens when the user is a teacher or admin.
+            project = Project.objects.get(project_id=project_id)
+            return JsonResponse({"id": project.project_id,
+                                 "name": project.name,
+                                 "deadline": project.deadline,
+                                 "description": project.description}, status=200)
+        elif request.user.student:
+            # When the user is a student.
+            student = request.user.student
+            if student.projects.filter(project_id=project_id).exists():
+                project = Project.objects.get(project_id=project_id)
+
+                return JsonResponse({"id": project.project_id,
+                                     "name": project.name,
+                                     "deadline": project.deadline,
+                                     "description": project.description}, status=200)
+            else:
+                return JsonResponse({"message": "Project not found"}, status=404)
+        else:
+            # User isn't recognized.
+            return JsonResponse({"message": "User role not recognized"}, status=403)
