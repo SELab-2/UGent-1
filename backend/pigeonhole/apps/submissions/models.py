01ff00cd72737b88727a4f2f99afef1fb950c6d1
Reinhard <reinharddp@gmail.com>
2024-03-02 18:25:29 +0100

    wrong fix so it works

--- backend/pigeonhole/apps/submissions/models.py
+++ backend/pigeonhole/apps/submissions/models.py
@@ -7,7 +7,7 @@ from backend.pigeonhole.apps.groups.models import Group
 # Create your models here.
 class Submissions(models.Model):
     submission_id = models.BigAutoField(primary_key=True)
-    group_id = models.ForeignKey(Group, on_delete=models.CASCADE)
+    group_id = models.ForeignKey(Group, on_delete=models.CASCADE, null=True)  # TODO: can't be null
     submission_nr = models.IntegerField()
     file = models.FileField(upload_to='uploads/' + str(group_id) + '/' + str(submission_nr) + '/')
     timestamp = models.DateTimeField(auto_now_add=True)
