ad541296a9004faa4c476fbfb78953e04a73e42e
robinpdev <robinparet22@gmail.com>
2024-03-02 11:28:19 +0100

    fix wrong path of env file

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 source /home/selab/hosting/.env.prod
-docker compose -f docker-compose.prod.yml --env-file /home/selab/hosting/.env.prod build
-docker compose -f docker-compose.prod.yml --env-file /home/selab/hosting/.env.prod up -d --force-recreate --remove-orphans
+docker compose -f docker-compose.prod.yml --env-file /home/selab2/hosting/.env.prod build
+docker compose -f docker-compose.prod.yml --env-file /home/selab2/hosting/.env.prod up -d --force-recreate --remove-orphans
