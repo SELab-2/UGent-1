5d81e5ea5940b3dcaf060116f4df2a111022895e
robinpdev <robinparet22@gmail.com>
2024-03-02 11:18:51 +0100

    deploy test

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-cd /home/selab2/hosting/UGent-1
-
-set -a
-source ./.env.prod
-docker compose -f docker-compose.prod.yml --env-file .env.prod build
-docker compose -f docker-compose.prod.yml --env-file .env.prod up -d --force-recreate --remove-orphans
+source /home/selab/hosting/.env.prod
+docker compose -f docker-compose.prod.yml --env-file /home/selab/hosting/.env.prod build
+docker compose -f docker-compose.prod.yml --env-file /home/selab/hosting/.env.prod up -d --force-recreate --remove-orphans
