b5b24f2b97fe6318cd4368bbd5563e410df1cbb6
gilles-arnout <gilles.arnout@gmail.com>
2024-03-02 18:16:37 +0100

    basic login page

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -4,11 +4,11 @@ import LoginForm from '@/components/ui/LoginForm';
 
 
 const Home = () => {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <LoginForm />
-    </main>
-  );
+    return (
+        <main className="flex min-h-screen flex-col items-center justify-between p-24">
+            <LoginForm/>
+        </main>
+    );
 };
 
 export default Home;
--- frontend/src/components/ui/LoginForm.tsx
+++ frontend/src/components/ui/LoginForm.tsx
@@ -1,16 +1,50 @@
 // src/app/components/LoginForm.tsx
 
-import { Button } from "@/components/ui/button"
+import React, {useState} from 'react';
 
 const LoginForm = () => {
-  return (
-    <div>
-      <h1>Pigeonhole</h1>
-      <div>
-      <Button>Click me</Button>
-      </div>
-    </div>
-  );
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+
+    const handleLogin = () => {
+        // Implement your login logic here
+    };
+
+    const handleCASLogin = () => {
+        // Implement your CAS login logic here
+    };
+
+    return (
+        <div>
+            <h2>Pigeon Hole</h2>
+            <form>
+                <label>
+                    Username:
+                    <input
+                        type="text"
+                        value={username}
+                        onChange={(e) => setUsername(e.target.value)}
+                    />
+                </label>
+                <br/>
+                <label>
+                    Password:
+                    <input
+                        type="password"
+                        value={password}
+                        onChange={(e) => setPassword(e.target.value)}
+                    />
+                </label>
+                <br/>
+                <button type="button" onClick={handleLogin}>
+                    Login
+                </button>
+                <button type="button" onClick={handleCASLogin}>
+                    Login using CAS
+                </button>
+            </form>
+        </div>
+    );
 };
 
 export default LoginForm;
