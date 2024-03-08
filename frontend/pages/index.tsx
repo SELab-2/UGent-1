f224e8a62cfe730d3d4478a8e481a603d1522b48
Thibaud Collyn <thibaudcollyn@gmail.com>
2024-03-08 22:33:43 +0100

    Added Material UI dependence and tested it out with some buttons.

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -1,12 +1,14 @@
 // pages/index.tsx
 import React from 'react';
 import LoginForm from '@/components/ui/LoginForm';
+import LoginCard from '@/components/ui/LoginCard';
 
 
 const Home = () => {
     return (
         <main className="flex min-h-screen flex-col items-center justify-between p-24">
             <LoginForm/>
+            <LoginCard/>
         </main>
     );
 };
