2baf464d40359d9e7505312fc5865d7363aac70c
avoyen <alexander.vanoyen@ugent.be>
2024-03-08 18:45:48 +0100

    Project viewset and custom permission class

--- backend/pigeonhole/apps/projects/views.py
+++ backend/pigeonhole/apps/projects/views.py
@@ -1,45 +1,50 @@
-from django.http import JsonResponse
+from rest_framework import status
+from rest_framework import viewsets
 from rest_framework.permissions import IsAuthenticated
-from rest_framework.views import APIView
+from rest_framework.response import Response
 
-from models import Project
+from models import Project, ProjectSerializer
+from permissions import CanAccessProject
 
 
-class ProjectAPIView(APIView):
-    permission_classes = [IsAuthenticated]
+class ProjectViewSet(viewsets.ModelViewSet):
+    queryset = Project.objects.all()
+    serializer_class = ProjectSerializer
+    permission_classes = [IsAuthenticated & CanAccessProject]
 
-    def get(self, request, subject_id, project_id):
-        # TODO still figuring out how to implement views properly.
-        if request.user.teacher:
-            # TODO should be able to get every project as an admin, not as a teacher. (check the is_admin field)
-            # This happens when the user is a teacher or admin.
-            project = Project.objects.get(project_id=project_id)
-            return JsonResponse({"id": project.project_id,
-                                 "name": project.name,
-                                 "deadline": project.deadline,
-                                 "description": project.description}, status=200)
-        elif request.user.student:
-            # When the user is a student.
-            student = request.user.student
-            if student.projects.filter(project_id=project_id).exists():
-                project = Project.objects.get(project_id=project_id)
-
-                return JsonResponse({"id": project.project_id,
-                                     "name": project.name,
-                                     "deadline": project.deadline,
-                                     "description": project.description}, status=200)
-            else:
-                return JsonResponse({"message": "Project not found"}, status=404)
+    def create(self, request, *args, **kwargs):
+        serializer = ProjectSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
         else:
-            # User isn't recognized.
-            return JsonResponse({"message": "User role not recognized"}, status=403)
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-    def put(self, request, subject_id, project_id):
-        if request.user.teacher:
-            # update project data, such as name, description, ...
-            project = Project.objects.get(project_id=project_id)
-        elif request.user.student:
-            student = request.user.student
-            project = Project.objects.get(project_id=project_id)
+    def destroy(self, request, *args, **kwargs):
+        subject_id = kwargs.get('subject_id')
+        project_id = kwargs.get('pk')
+        project = Project.objects.get(pk=project_id)
+        project.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
 
+    def retrieve(self, request, *args, **kwargs):
+        subject_id = kwargs.get('subject_id')
+        project_id = kwargs.get('pk')
 
+        serializer = ProjectSerializer(instance=Project.objects.get(pk=project_id), many=False)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    def update(self, request, *args, **kwargs):
+        subject_id = kwargs.get('subject_id')
+        project_id = kwargs.get('pk')
+        project = Project.objects.get(pk=project_id)
+        serializer = ProjectSerializer(project, data=request.data)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    def archive(self, request, *args, **kwargs):
+        # Not sure what to do here yet
+        subject_id = kwargs.get('subject_id')
+        project_id = kwargs.get('pk')
+        serializer = ProjectSerializer(instance=Project.objects.get(pk=project_id), many=False)
+        return Response(serializer, status=status.HTTP_200_OK)
