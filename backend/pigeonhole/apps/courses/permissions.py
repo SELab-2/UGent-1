25e4146d3d369806c7048f2022ce61f19758d5a0
Reinhard <reinharddp@gmail.com>
2024-03-09 22:02:51 +0100

    course permissions fixed

--- backend/pigeonhole/apps/courses/permissions.py
+++ backend/pigeonhole/apps/courses/permissions.py
@@ -4,31 +4,27 @@ from backend.pigeonhole.apps.users.models import Student, Teacher
 
 class CourseUserPermissions(permissions.BasePermission):
     def has_permission(self, request, view):
-        if not request.user.is_authenticated:
-            return False
-
-        if request.user.is_admin:
+        if request.user.is_superuser:
+            return True
+        if isinstance(request.user, Teacher):
             return True
 
-        if request.user.is_teacher:
-            return True
-
-        if request.user.is_student:
+        if isinstance(request.user, Student):
             return view.action in ['list', 'retrieve']
 
         return False
 
     def has_object_permission(self, request, view, obj):
-        if not request.user.is_authenticated:
-            return False
-
-        if request.user.is_admin:
+        if request.user.is_superuser:
             return True
+        if isinstance(request.user, Teacher):
+            if request.user.is_admin:
+                return True
+            elif Teacher.objects.filter(id=request.user.id, course=obj).exists():
+                return True
+            return view.action in ['list', 'retrieve']
 
-        if request.user.is_teacher:
-            return Teacher.objects.filter(id=request.user.id, course=obj).exists()
-
-        if request.user.is_student:
-            return Student.objects.filter(id=request.user.id, course=obj).exists() and view.action in ['list', 'retrieve']
+        if isinstance(request.user, Student):
+            return view.action in ['list', 'retrieve']
 
         return False
