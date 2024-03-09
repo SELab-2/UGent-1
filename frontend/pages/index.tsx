fb2692fc2a7336c55ac4d35764b129daca2b8de0
gilles-arnout <gilles.arnout@gmail.com>
2024-03-09 14:25:58 +0100

    styled login page

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -1,14 +1,13 @@
 // pages/index.tsx
 import React from 'react';
-import LoginForm from '@/components/ui/LoginForm';
 import LoginCard from '@/components/ui/LoginCard';
+import LoginForm from '@/components/ui/LoginForm';
 
 
 const Home = () => {
     return (
         <main className="flex min-h-screen flex-col items-center justify-between p-24">
             <LoginForm/>
-            <LoginCard/>
         </main>
     );
 };
