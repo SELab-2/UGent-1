fb9452fc9bd0066c45b4a58b7825abfb2bf6fdbe
Axel Lorreyne <axellorreyne2@gmail.com>
2024-03-10 05:06:09 +0100

    fix route

--- backend/pigeonhole/apps/users/urls.py
+++
@@ -1,8 +0,0 @@
-from rest_framework.routers import SimpleRouter
-
-from backend.pigeonhole.apps.users.viewsets import LoginViewSet
-
-routes = SimpleRouter()
-
-# AUTHENTICATION
-routes.register(r'auth/login', LoginViewSet, basename='auth-login')
--- backend/pigeonhole/urls.py
+++ backend/pigeonhole/urls.py
@@ -1,11 +1,13 @@
 from django.urls import include, path
 from rest_framework import routers
 
+from backend.pigeonhole.apps.users.viewsets import LoginViewSet
 from backend.testapi import views
 
 router = routers.DefaultRouter()
 router.register(r'users', views.UserViewSet)
 router.register(r'groups', views.GroupViewSet)
+router.register(r'login', LoginViewSet, basename='auth-login')
 
 # Wire up our API using automatic URL routing.
 # Additionally, we include login URLs for the browsable API.
