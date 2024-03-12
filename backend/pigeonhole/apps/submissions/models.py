6e97962a6c39609bde7174f511d6f3cf304ea607
rdyselinck <rune.dyselinck@ugent.be>
2024-03-12 17:30:51 +0100

    Submissions untested

--- backend/pigeonhole/apps/submissions/models.py
+++ backend/pigeonhole/apps/submissions/models.py
@@ -9,19 +9,19 @@ from backend.pigeonhole.apps.groups.models import Group
 class Submissions(models.Model):
     submission_id = models.BigAutoField(primary_key=True)
     group_id = models.ForeignKey(Group, on_delete=models.CASCADE, blank=False)
-    submission_nr = models.IntegerField()
+    submission_nr = models.IntegerField(blank=True)
     file = models.FileField(upload_to='uploads/submissions/files/' +
                                       str(group_id) + '/' + str(submission_nr) + '/',
                             null=True, blank=False, max_length=255)
     timestamp = models.DateTimeField(auto_now_add=True)
     output_test = models.FileField(upload_to='uploads/submissions/outputs/' +
                                              str(group_id) + '/' + str(submission_nr) +
-                                             '/output_test/', null=True, blank=False,
+                                             '/output_test/', null=True, blank=True,
                                    max_length=255)
 
     objects = models.Manager()
 
-    # submission_nr is automatically assigned and unique per group, and 
+    # submission_nr is automatically assigned and unique per group, and
     # increments
     def save(self, force_insert=False, force_update=False, using=None, update_fields=None):
         if not self.submission_id:
@@ -32,6 +32,15 @@ class Submissions(models.Model):
         super().save(force_insert=force_insert, force_update=force_update, using=using, update_fields=update_fields)
 
 class SubmissionsSerializer(serializers.ModelSerializer):
+    submission_nr = serializers.IntegerField(read_only=True)
+    output_test = serializers.FileField(read_only=True)
+
     class Meta:
         model = Submissions
-        fields = ['submission_id', 'group_id', 'file', 'timestamp', 'submission_nr', 'output_test']
+        fields = ['submission_id', 'file', 'timestamp', 'submission_nr', 'output_test', 'group_id']
+        read_only_fields = ['group_id']
+
+    def save(self, **kwargs):
+        group_id = self.context['view'].kwargs.get('group_id')
+        self.validated_data['group_id'] = Group.objects.get(pk=group_id)
+        return super().save(**kwargs)
