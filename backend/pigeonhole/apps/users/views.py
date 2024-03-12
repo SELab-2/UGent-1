3908805a7fc00db82914479750761ec893997f81
Reinhard <reinharddp@gmail.com>
2024-03-12 17:51:31 +0100

    user api and permissions

--- backend/pigeonhole/apps/users/views.py
+++ backend/pigeonhole/apps/users/views.py
@@ -3,12 +3,13 @@ from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 
 from backend.pigeonhole.apps.users.models import User, UserSerializer
+from .permissions import UserPermissions
 
 
 class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerializer
-    permission_classes = [IsAuthenticated]
+    permission_classes = [IsAuthenticated, UserPermissions]
 
     def list(self, request, *args, **kwargs):
         serializer = UserSerializer(self.queryset, many=True)
@@ -24,3 +25,35 @@ class UserViewSet(viewsets.ModelViewSet):
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         else:
             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def update(self, request, *args, **kwargs):
+        user_id = kwargs.get('pk')
+        user = User.objects.get(pk=user_id)
+        serializer = UserSerializer(user, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def partial_update(self, request, *args, **kwargs):
+        user_id = kwargs.get('pk')
+        user = User.objects.get(pk=user_id)
+        serializer = UserSerializer(user, data=request.data, partial=True)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def destroy(self, request, *args, **kwargs):
+        user_id = kwargs.get('pk')
+        user = User.objects.get(pk=user_id)
+        user.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+    def retrieve(self, request, *args, **kwargs):
+        user_id = kwargs.get('pk')
+        user = User.objects.get(pk=user_id)
+        serializer = UserSerializer(user, many=False)
+        return Response(serializer.data, status=status.HTTP_200_OK)
