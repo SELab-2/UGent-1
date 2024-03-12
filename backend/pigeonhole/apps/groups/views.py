d9c07850527b98951879ca50df53757741587d4d
avoyen <alexander.vanoyen@ugent.be>
2024-03-12 23:29:46 +0100

    group nr url

--- backend/pigeonhole/apps/groups/views.py
+++ backend/pigeonhole/apps/groups/views.py
@@ -6,6 +6,7 @@ from rest_framework.response import Response
 from backend.pigeonhole.apps.courses.models import Course
 from backend.pigeonhole.apps.groups.models import Group, GroupSerializer
 from backend.pigeonhole.apps.projects.models import Project
+from .permission import CanAccessGroup
 
 
 # TODO tests for score/max_score
@@ -14,23 +15,31 @@ from backend.pigeonhole.apps.projects.models import Project
 class GroupViewSet(viewsets.ModelViewSet):
     queryset = Group.objects.all()
     serializer_class = GroupSerializer
-    permission_classes = [IsAuthenticated]
+    permission_classes = [IsAuthenticated and CanAccessGroup]
+    lookup_field = 'group_nr'
 
     def list(self, request, *args, **kwargs):
+        course_id = kwargs.get('course_id')
+        project_id = kwargs.get('project_id')
+        # Check whether the course exists
+        get_object_or_404(Course, course_id=course_id)
+        # Check whether the project exists
+        get_object_or_404(Project, pk=project_id)
 
-        serializer = GroupSerializer(self.queryset, many=True)
+        queryset = self.queryset.filter(project_id=project_id)
+        serializer = self.get_serializer(queryset, many=True)
         return Response(serializer.data, status=status.HTTP_200_OK)
 
     def retrieve(self, request, *args, **kwargs):
         course_id = kwargs.get('course_id')
         project_id = kwargs.get('project_id')
-        group_id = kwargs.get('pk')
+        group_nr = kwargs.get('group_nr')
 
         # Check whether the course exists
         get_object_or_404(Course, course_id=course_id)
         # Check whether the project exists
         get_object_or_404(Project, pk=project_id)
-        group = get_object_or_404(Group, group_id=group_id)
+        group = get_object_or_404(Group, project_id=project_id, group_nr=group_nr)
         user = request.user
         serializer = GroupSerializer(instance=group, many=False)
         if user.is_superuser or user.is_staff or user.is_teacher:
@@ -42,10 +51,10 @@ class GroupViewSet(viewsets.ModelViewSet):
     def update(self, request, *args, **kwargs):
         course_id = kwargs.get('course_id')
         project_id = kwargs.get('project_id')
-        group_id = kwargs.get('pk')
+        group_nr = kwargs.get('group_nr')
         get_object_or_404(Course, course_id=course_id)
         get_object_or_404(Project, project_id=project_id)
-        group = get_object_or_404(Group, group_id=group_id)
+        group = get_object_or_404(Group, group_nr=group_nr)
         serializer = GroupSerializer(instance=group, data=request.data, partial=False)
         if serializer.is_valid():
             serializer.save()
@@ -54,17 +63,17 @@ class GroupViewSet(viewsets.ModelViewSet):
     def partial_update(self, request, *args, **kwargs):
         course_id = kwargs.get('course_id')
         project_id = kwargs.get('project_id')
-        group_id = kwargs.get('pk')
+        group_nr = kwargs.get('group_nr')
         get_object_or_404(Course, course_id=course_id)
         get_object_or_404(Project, project_id=project_id)
-        group = get_object_or_404(Group, group_id=group_id)
+        group = get_object_or_404(Group, group_nr=group_nr)
         serializer = GroupSerializer(instance=group, data=request.data, partial=True)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data, status=status.HTTP_200_OK)
 
     def create(self, request, *args, **kwargs):
-        return Response({"message": "You can't creat groups"}, status=status.HTTP_400_BAD_REQUEST)
+        return Response({"message": "You can't create groups"}, status=status.HTTP_400_BAD_REQUEST)
 
     def destroy(self, request, *args, **kwargs):
-        return Response({"message": "You can't creat groups"}, status=status.HTTP_400_BAD_REQUEST)
+        return Response({"message": "You can't delete groups"}, status=status.HTTP_400_BAD_REQUEST)
