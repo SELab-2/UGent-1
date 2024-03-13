5a2e0a491c696e00ab7eb80f58c944c695f1bdaa
PJDeSmijter <pieterjan.desmijter@gmail.com>
2024-03-13 20:32:57 +0100

    cleanup groups

--- backend/pigeonhole/apps/groups/views.py
+++ backend/pigeonhole/apps/groups/views.py
@@ -15,29 +15,3 @@ class GroupViewSet(viewsets.ModelViewSet):
     queryset = Group.objects.all()
     serializer_class = GroupSerializer
     permission_classes = [IsAuthenticated and CanAccessGroup]
-
-    """
-    def list(self, request, *args, **kwargs):
-        course_id = kwargs.get('course_id')
-        project_id = kwargs.get('project_id')
-
-        # Check whether the course exists
-        get_object_or_404(Course, course_id=course_id)
-        # Check whether the project exists
-        project = get_object_or_404(Project, project=project_id)
-
-        # Ensure the project is associated with the course
-        if project.course_id != int(course_id):
-            return Response({'detail': 'Project not found in the specified course.'}, status=status.HTTP_404_NOT_FOUND)
-
-        queryset = self.queryset.filter(project_id=project_id)
-        serializer = self.get_serializer(queryset, many=True)
-
-        if request.user.is_student:
-            for group in serializer.data:
-                if request.user.id not in group["users"]:
-                    del group["final_score"]
-                    del group["feedback"]
-
-        return Response(serializer.data, status=status.HTTP_200_OK)
-    """
