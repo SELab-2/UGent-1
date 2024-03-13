22b392f3b2d397a7636a1887341842764623bc11
PJDeSmijter <pieterjan.desmijter@gmail.com>
2024-03-13 22:55:38 +0100

    fix submissions

--- backend/pigeonhole/apps/submissions/views.py
+++ backend/pigeonhole/apps/submissions/views.py
@@ -14,25 +14,3 @@ class SubmissionsViewset(viewsets.ModelViewSet):
     queryset = Submissions.objects.all()
     serializer_class = SubmissionsSerializer
     permission_classes = [IsAuthenticated & CanAccessSubmission]
-
-    @action(detail=False, methods=['POST'])
-    def submit(self, request, *args, **kwargs):
-        submission = self.get_submission()
-        serializer = SubmissionsSerializer(submission, data=request.data)
-        serializer.is_valid(raise_exception=True)
-        serializer.save()
-        return Response(serializer.data, status=status.HTTP_201_CREATED)
-
-    @action(detail=True, methods=['GET'])
-    def get_submission(self, request, *args, **kwargs):
-        submission = self.get_object()
-        return Response(SubmissionsSerializer(submission).data, status=status.HTTP_200_OK)
-
-    @action(detail=False, methods=['GET'])
-    def get_all_submissions(self, request, *args, **kwargs):
-        user = request.user
-        groups = Group.objects.filter(users=user)
-        submissions = Submissions.objects.filter(group_id__in=groups)
-        if not submissions:
-            return Response({"message": "No submissions found"}, status=status.HTTP_404_NOT_FOUND)
-        return Response(SubmissionsSerializer(submissions, many=True).data, status=status.HTTP_200_OK)
