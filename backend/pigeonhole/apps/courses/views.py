46abe051c2037550b26cfcfc8a5b03987a57a56c
PJDeSmijter <pieterjan.desmijter@gmail.com>
2024-03-13 12:10:30 +0100

    Revert "fix courses"
    This reverts commit 727a8c6b71f6cda4e20356e0e1c9215071c5fd8d.

--- backend/pigeonhole/apps/courses/views.py
+++ backend/pigeonhole/apps/courses/views.py
@@ -3,33 +3,72 @@ from rest_framework.decorators import action
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 
-from .models import Course
+from backend.pigeonhole.apps.users.models import User
+from .models import Course, CourseSerializer
 from .permissions import CourseUserPermissions
-from backend.pigeonhole.apps.courses.models import CourseSerializer
 
 
 class CourseViewSet(viewsets.ModelViewSet):
-    queryset = Course.objects.all()  # Add this line to specify the queryset
+    queryset = Course.objects.all()
     serializer_class = CourseSerializer
     permission_classes = [IsAuthenticated, CourseUserPermissions]
 
-    @action(detail=True, methods=['post'])
-    def join_course(self, request, *args, **kwargs):
-        course = self.get_object()
-        user = request.user
-        user.course.add(course)
-        return Response(status=status.HTTP_200_OK)
+    def create(self, request, *args, **kwargs):
+        serializer = CourseSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-    @action(detail=False, methods=['GET'])
-    def get_selected_courses(self, request, *args, **kwargs):
-        user = request.user
-        courses = user.course.all()
-        serializer = CourseSerializer(courses, many=True)
+    def update(self, request, *args, **kwargs):
+        course_id = kwargs.get('pk')
+        course = Course.objects.get(pk=course_id)
+        serializer = CourseSerializer(course, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def destroy(self, request, *args, **kwargs):
+        course_id = kwargs.get('pk')
+        course = Course.objects.get(pk=course_id)
+        course.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+    def list(self, request, *args, **kwargs):
+        serializer = CourseSerializer(self.queryset, many=True)
         return Response(serializer.data, status=status.HTTP_200_OK)
 
-    def get_queryset(self):
-        if self.action == 'list':
-            return Course.objects.all()
-        elif self.action == 'get_selected_courses':
-            return self.request.user.course.all()
-        return Course.objects.none()
+    def my_list(self, request, *args, **kwargs):
+        if request.user.is_admin or request.user.is_superuser:
+            serializer = CourseSerializer(self.queryset, many=True)
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        if request.user.is_teacher or request.user.is_student:
+            courses = User.objects.get(id=request.user.id).course.all()
+            serializer = CourseSerializer(courses, many=True)
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        serializer = CourseSerializer(self.queryset, many=True)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def retrieve(self, request, *args, **kwargs):
+        course_id = kwargs.get('pk')
+        course = Course.objects.get(pk=course_id)
+        serializer = CourseSerializer(course, many=False)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    def partial_update(self, request, *args, **kwargs):
+        course_id = kwargs.get('pk')
+        course = Course.objects.get(pk=course_id)
+        serializer = CourseSerializer(course, data=request.data, partial=True)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    @action(detail=True, methods=['post'])
+    def join_course(self, request, *args, **kwargs):
+        course_id = kwargs.get('pk')
+        course = Course.objects.get(pk=course_id)
+        user = User.objects.get(id=request.user.id)
+        user.course.add(course)
+        return Response(status=status.HTTP_200_OK)
