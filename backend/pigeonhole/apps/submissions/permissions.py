9beebc88c03e6778a5b6a574e8a62f0735a0fc55
avoyen <alexander.vanoyen@ugent.be>
2024-03-13 23:28:48 +0100

    submission permissions

--- backend/pigeonhole/apps/submissions/permissions.py
+++ backend/pigeonhole/apps/submissions/permissions.py
@@ -2,35 +2,40 @@ from rest_framework import permissions, status
 from rest_framework.response import Response
 
 from backend.pigeonhole.apps.groups.models import Group
-from backend.pigeonhole.apps.projects.models import Project
 
 
 class CanAccessSubmission(permissions.BasePermission):
     # Custom permission class to determine if the currect user has access
     # to the submission data.
     def has_permission(self, request, view):
-        if view.action in ['create', 'list']:
-            return False
         user = request.user
-        group_id = int(view.kwargs.get('pk'))
-        if not Group.objects.filter(group_id=group_id).exists():
-            if user.is_admin or user.is_superuser:
-                return Response(status=status.HTTP_404_NOT_FOUND)
+        if view.action in ['list']:
             return False
-
-        project_id = Group.objects.get(group_id=group_id).project_id
-        if not Project.objects.filter(project_id=project_id).exists():
+        elif view.action in ['create']:
+            if user.is_student:
+                group_id = request.data.get('group_id')
+                if not Group.objects.filter(group_id=group_id).exists():
+                    if user.is_admin or user.is_superuser:
+                        return Response(status=status.HTTP_404_NOT_FOUND)
+                    return False
+                group = Group.objects.get(group_id=group_id)
+                if user in group.user:
+                    return True
+                else:
+                    return False
+        else:
+            group_id = int(view.kwargs.get('pk'))
+            if not Group.objects.filter(group_id=group_id).exists():
+                if user.is_admin or user.is_superuser:
+                    return Response(status=status.HTTP_404_NOT_FOUND)
+                return False
+            group = Group.objects.get(group_id=group_id)
             if user.is_admin or user.is_superuser:
-                return Response(status=status.HTTP_404_NOT_FOUND)
+                return True
+            elif user.is_teacher:
+                if user in group.user:
+                    return view.action in ['retrieve']
+            elif user.is_student:
+                if user in group.user:
+                    return view.action in ['retrieve', 'create']
             return False
-
-        course_id = Project.objects.get(project_id=project_id).course_id.course_id
-        if user.is_admin or user.is_superuser:
-            return True
-        elif user.is_teacher:
-            if user.course.filter(course_id=course_id).exists():
-                return view.action in ['retrieve', 'get_submissions', 'update', 'partial_update']
-        elif user.is_student:
-            if user.course.filter(course_id=course_id).exists():
-                return view.action in ['retrieve', 'join', 'leave']
-        return False
