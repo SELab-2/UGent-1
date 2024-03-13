d73ed72c3a37d29d1a44858469eddebbe12b0491
PJDeSmijter <pieterjan.desmijter@gmail.com>
2024-03-13 23:01:29 +0100

    Revert "Merge branch 'api-working' of https://github.com/SELab-2/UGent-1 into api-working"
    This reverts commit 325037e102312c76505e127df3c559db407c0ae6, reversing
    changes made to 22b392f3b2d397a7636a1887341842764623bc11.

--- backend/pigeonhole/apps/groups/permission.py
+++ backend/pigeonhole/apps/groups/permission.py
@@ -1,35 +1,18 @@
-from requests import Response
-from rest_framework import permissions, status
-
-from backend.pigeonhole.apps.groups.models import Group
-from backend.pigeonhole.apps.projects.models import Project
+from rest_framework import permissions
 
 
 class CanAccessGroup(permissions.BasePermission):
     # Custom user class to check if the user can join a group.
     def has_permission(self, request, view):
-        if view.action in ['create', 'list']:
-            return False
         user = request.user
-        group_id = int(view.kwargs.get('pk'))
-        if not Group.objects.filter(group_id=group_id).exists():
-            if user.is_admin or user.is_superuser:
-                return Response(status=status.HTTP_404_NOT_FOUND)
-            return False
-
-        project_id = Group.objects.get(group_id=group_id).project_id
-        if not Project.objects.filter(project_id=project_id).exists():
-            if user.is_admin or user.is_superuser:
-                return Response(status=status.HTTP_404_NOT_FOUND)
-            return False
-
-        course_id = Project.objects.get(project_id=project_id).course_id.course_id
+        course_id = view.kwargs.get('course_id')
         if user.is_admin or user.is_superuser:
             return True
         elif user.is_teacher:
             if user.course.filter(course_id=course_id).exists():
-                return view.action in ['retrieve', 'get_submissions', 'update', 'partial_update']
+                return view.action in ['retrieve', 'get_submissions']
         elif user.is_student:
             if user.course.filter(course_id=course_id).exists():
-                return view.action in ['retrieve', 'join', 'leave']
+                return view.action in ['retrieve', 'join',
+                                       'leave', 'get_submissions']
         return False
