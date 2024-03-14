1e4b30775617bfb5dac9773657cdefa05918d69d
robinpdev <robinparet22@gmail.com>
2024-03-14 15:58:45 +0100

    make environment variables work on production frontend

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
-source /home/selab/hosting/.env.prod
+source /home/selab2/hosting/.env.prod
+cp /home/selab2/hosting/.env.prod ./frontend/.env.local
 docker compose -f docker-compose.prod.yml --env-file /home/selab2/hosting/.env.prod build
 docker compose -f docker-compose.prod.yml --env-file /home/selab2/hosting/.env.prod up -d --force-recreate --remove-orphans
