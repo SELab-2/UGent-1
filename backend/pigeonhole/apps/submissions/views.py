36bd5408b454816368f10ab590b8c8cedba0a065
Axel Lorreyne <axellorreyne2@gmail.com>
2024-03-14 02:49:24 +0100

    add submission 404 and deadline checks

--- backend/pigeonhole/apps/submissions/views.py
+++ backend/pigeonhole/apps/submissions/views.py
@@ -1,9 +1,12 @@
+from datetime import datetime
+
 from rest_framework import viewsets, status
 from rest_framework.decorators import action
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 
 from backend.pigeonhole.apps.groups.models import Group
+from backend.pigeonhole.apps.projects.models import Project
 from backend.pigeonhole.apps.submissions.models import Submissions, SubmissionsSerializer
 from backend.pigeonhole.apps.submissions.permissions import CanAccessSubmission
 
@@ -21,6 +24,18 @@ class SubmissionsViewset(viewsets.ModelViewSet):
         submission = self.get_submission()
         serializer = SubmissionsSerializer(submission, data=request.data)
         serializer.is_valid(raise_exception=True)
+
+        group = Group.objects.get(id=serializer.group_id)
+        if not group:
+            return Response({"message": "Group not found"}, status=status.HTTP_404_NOT_FOUND)
+
+        project = Project.objects.get(id=group.project_id)
+        if not project:
+            return Response({"message": "Project not found"}, status=status.HTTP_404_NOT_FOUND)
+
+        if datetime.now() > project.deadline:
+            return Response({"message": "Deadline expired"}, status=status.HTTP_410_GONE)
+
         serializer.save()
         return Response(serializer.data, status=status.HTTP_201_CREATED)
 
