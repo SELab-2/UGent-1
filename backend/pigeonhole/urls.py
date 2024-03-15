85ba697161943ba0f5415e116c00157e2f80d236
robinpdev <robinparet22@gmail.com>
2024-03-15 10:25:32 +0100

    add logout button

--- backend/pigeonhole/urls.py
+++ backend/pigeonhole/urls.py
@@ -2,7 +2,8 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.shortcuts import redirect
-from django.urls import include, path as urlpath
+from django.urls import include, reverse_lazy, path as urlpath
+from django.contrib.auth import views as auth_views
 from drf_yasg import openapi
 from drf_yasg.views import get_schema_view
 from rest_framework import routers, permissions
@@ -48,6 +49,7 @@ urlpatterns = [
                   urlpath('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
                   urlpath("admin/", admin.site.urls),
                   urlpath('auth/login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+                  urlpath('auth/logout', auth_views.LogoutView.as_view(next_page=settings.FRONTEND_URL), name='authlogout'),
                   urlpath('auth/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
                   urlpath('microsoft/', include('microsoft_auth.urls', namespace='microsoft')),
                   urlpath('redirect/<path:path>', to_frontend, name='redirect'),
