46fed3d7b789f4254571fd9afc6c6927a395345d
gilles-arnout <gilles.arnout@gmail.com>
2024-03-23 17:43:31 +0100

    quick fix

--- frontend/lib/clientSideHook.tsx
+++ frontend/lib/clientSideHook.tsx
@@ -1,9 +1,8 @@
-import { useState, useEffect } from 'react';
+import {useEffect, useState} from 'react';
 import initTranslations from "../app/i18n";
 
-// This is a custom hook for client-side initialization
 export const useClientSideTranslations = (locale: string, namespaces: string[]) => {
-    const [t, setT] = useState<(key: string) => string>(() => key => key); // Dummy function to start with
+    const [t, setT] = useState<(key: string) => string>(() => (key: string) => key);
 
     useEffect(() => {
         let isMounted = true;
