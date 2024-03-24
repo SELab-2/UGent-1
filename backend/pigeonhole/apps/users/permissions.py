2b7b135249eff8d61a4ee32a7f4c2784d25156c4
PJDeSmijter <pieterjan.desmijter@gmail.com>
2024-03-24 20:54:30 +0100

    user model addCourse, removeCourse

--- backend/pigeonhole/apps/users/permissions.py
+++ backend/pigeonhole/apps/users/permissions.py
@@ -4,13 +4,19 @@ from rest_framework import permissions
 class UserPermissions(permissions.BasePermission):
     def has_permission(self, request, view):
         if request.user.is_admin or request.user.is_superuser:
-            return True  # TODO can admins destroy each other?
+            return True
 
         if request.user.is_teacher or request.user.is_student:
-            if view.action in ['list', 'retrieve']:  # TODO: can teachers create and destroy users?
+            if view.action in ["list", "retrieve"]:
                 return True
             # user can only update their own user
-            elif view.action in ['update', 'partial_update'] and request.user.pk == int(view.kwargs['pk']):
+            elif view.action in ["update", "partial_update"] and request.user.pk == int(
+                view.kwargs["pk"]
+            ):
                 return True
-
+            elif view.action in ["add_course_to_user", "remove_course_from_user"]:
+                return request.user.is_teacher or (
+                    request.user.is_student
+                    and request.user.pk == int(view.kwargs["pk"])
+                )
         return False
--- backend/pigeonhole/apps/users/views.py
+++ backend/pigeonhole/apps/users/views.py
@@ -1,5 +1,8 @@
 from rest_framework import viewsets
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.decorators import action
+from rest_framework.response import Response
+from rest_framework import status
 
 from backend.pigeonhole.apps.users.models import User, UserSerializer
 from .permissions import UserPermissions
@@ -9,3 +12,23 @@ class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerializer
     permission_classes = [IsAuthenticated, UserPermissions]
+
+    @action(detail=True, methods=["post"])
+    def add_course_to_user(self, request, pk=None):
+        user = self.get_object()
+        course_id = request.data.get("course_id")
+        user.course.add(course_id)
+        user.save()
+        return Response(
+            {"status": "Course added successfully"}, status=status.HTTP_200_OK
+        )
+
+    @action(detail=True, methods=["post"])
+    def remove_course_from_user(self, request, pk=None):
+        user = self.get_object()
+        course_id = request.data.get("course_id")
+        user.course.remove(course_id)
+        user.save()
+        return Response(
+            {"status": "Course removed successfully"}, status=status.HTTP_200_OK
+        )
