e752c8fc8450fa6d4ecff3392ae3016809726252
Reinhard <reinharddp@gmail.com>
2024-04-05 21:25:40 +0200

    colours and linting fixed

--- frontend/app/[locale]/course_create/page.tsx
+++ frontend/app/[locale]/course_create/page.tsx
@@ -4,12 +4,13 @@ import NavBar from "../components/NavBar";
 import Box from "@mui/material/Box";
 import initTranslations from "../../i18n";
 import {postForm} from '@lib/api';
+import ugent_banner from './ugent_banner.png'; // TODO move image to resources
 
 
-function CourseCreatePage({ params: { locale } }: { params: { locale: any } }) {
-    const [translations, setTranslations] = useState({ t: (key: any) => key });
-    const [selectedImage, setSelectedImage] = useState('./ugent_banner.png'); // TODO move image to resources
-
+function CourseCreatePage({params: {locale}}: { params: { locale: any } }) {
+    const [translations, setTranslations] = useState({t: (key: any) => key});
+    const [selectedImage, setSelectedImage] = useState(ugent_banner);
+    console.log(ugent_banner);
     useEffect(() => {
         const initialize = async () => {
             const translations = await initTranslations(locale, ['common']);
@@ -18,6 +19,16 @@ function CourseCreatePage({ params: { locale } }: { params: { locale: any } }) {
 
         initialize();
     }, [locale]);
+
+    const handleRemoveCourse = () => {
+        // deleteRequest("/courses/")
+        //     .then(response => {
+        //         console.log("Course removed successfully");
+        //     })
+        //     .catch(error => {
+        //         console.error("Error removing course:", error);
+        //     });
+    };
 
     const handleImageUpload = (event: any) => { //TODO should be able to select the right part of the image
         const imageFile = event.target.files[0];
@@ -35,44 +46,118 @@ function CourseCreatePage({ params: { locale } }: { params: { locale: any } }) {
     };
     // TODO is choicebox still needed?
     // TODO save and remove button don't work
-    // TODO better colours
     return (
         <div>
-            <NavBar />
-            <Box sx={{ marginTop: '64px', position: 'relative' }}>
-                <button style={{backgroundColor: 'red', color: 'white', padding: '8px 16px', border: 'none', borderRadius: '6px', cursor: 'pointer', fontFamily: 'Arial, sans-serif', position: 'absolute', top: '20px', right: '20px', fontSize: '16px' }}>Remove course</button>
-                <Box sx={{ marginLeft: '70px', marginRight: '70px', marginTop: '100px' }}>
+            <NavBar/>
+            <Box sx={{marginTop: '64px', position: 'relative'}}>
+                <button onClick={handleRemoveCourse} style={{
+                    backgroundColor: 'red',
+                    color: 'white',
+                    padding: '8px 16px',
+                    border: 'none',
+                    borderRadius: '6px',
+                    cursor: 'pointer',
+                    fontFamily: 'Arial, sans-serif',
+                    position: 'absolute',
+                    top: '20px',
+                    right: '20px',
+                    fontSize: '16px'
+                }}>Remove course
+                </button>
+                <Box sx={{marginLeft: '70px', marginRight: '70px', marginTop: '100px'}}>
                     <form onSubmit={postForm("/courses/")}>
-                        <Box sx={{ marginTop: '16px' }}>
-                            <label htmlFor="name" style={{ fontSize: '32px', fontFamily: 'Arial, sans-serif', color: 'darkblue', marginBottom: '-10px', display: 'block'  }}>{translations.t("Course name")}</label><br />
-                            <input type="text" id="name" name="name" required style={{ fontSize: '20px', fontFamily: 'Arial, sans-serif', borderRadius: '6px', height: '30px', width: '220px' }} />
+                        <Box sx={{marginTop: '16px'}}>
+                            <label htmlFor="name" style={{
+                                fontSize: '32px',
+                                fontFamily: 'Arial, sans-serif',
+                                marginBottom: '-10px',
+                                display: 'block'
+                            }}>{translations.t("Course name")}</label><br/>
+                            <input type="text" id="name" name="name" required style={{
+                                fontSize: '20px',
+                                fontFamily: 'Arial, sans-serif',
+                                borderRadius: '6px',
+                                height: '30px',
+                                width: '220px'
+                            }}/>
                         </Box>
-                        <Box sx={{ marginTop: '16px',borderRadius: '12px' }} style={{ height: '250px' }}>
-                            <label htmlFor="banner" style={{ fontSize: '32px', fontFamily: 'Arial, sans-serif', color: 'darkblue' }}>{translations.t("Banner")}</label><br />
-                            <img src={selectedImage} alt="Profile Image" style={{ width: '100%', height: '220px',objectFit: 'cover', objectPosition: 'center', borderRadius: '12px' }}/>
+                        <Box sx={{marginTop: '16px', borderRadius: '12px'}} style={{height: '250px'}}>
+                            <label htmlFor="banner" style={{
+                                fontSize: '32px',
+                                fontFamily: 'Arial, sans-serif',
+                                color: '#1E64C8'
+                            }}>{translations.t("Banner")}</label><br/>
+                            <img src={selectedImage} alt="Image" style={{
+                                width: '100%',
+                                height: '220px',
+                                objectFit: 'cover',
+                                objectPosition: 'center',
+                                borderRadius: '12px'
+                            }}/>
                         </Box>
                         <Box sx={{marginTop: '16px'}}>
-                            <label htmlFor="profileImage" style={{
+                            <label htmlFor="Image" style={{
                                 cursor: 'pointer',
-                                color: 'black', display: 'inline-block', padding: '8px 16px', border: '1px solid lightblue', borderRadius: '4px', backgroundColor: 'lightblue', color: 'darkblue', fontFamily: 'Arial, sans-serif' }}>
+                                display: 'inline-block',
+                                padding: '8px 16px',
+                                border: '1px solid lightblue',
+                                borderRadius: '4px',
+                                backgroundColor: 'lightblue',
+                                color: '#1E64C8',
+                                fontFamily: 'Arial, sans-serif'
+                            }}>
                                 Select image
-                                <input type="file" id="profileImage" name="profileImage" accept="image/*" onChange={handleImageUpload} style={{ display: 'none' }} />
+                                <input type="file" id="Image" name="Image" accept="image/*" onChange={handleImageUpload}
+                                       style={{display: 'none'}}/>
                             </label>
                         </Box>
-                        <Box sx={{ marginTop: '16px' }}>
-                            <label htmlFor="description" style={{ fontSize: '32px', fontFamily: 'Arial, sans-serif', color: 'darkblue', marginBottom: '-10px', display: 'block' }}>{translations.t("Description")}</label><br />
-                            <textarea id="description" name="description" rows={5} required style={{width: '100%', fontFamily: 'Arial, sans-serif', color: 'darkblue', borderRadius: '6px', padding: '10px' }}/>
+                        <Box sx={{marginTop: '16px'}}>
+                            <label htmlFor="description" style={{
+                                fontSize: '32px',
+                                fontFamily: 'Arial, sans-serif',
+                                color: '#1E64C8',
+                                marginBottom: '-10px',
+                                display: 'block'
+                            }}>{translations.t("Description")}</label><br/>
+                            <textarea id="description" name="description" rows={5} required style={{
+                                width: '100%',
+                                fontFamily: 'Arial, sans-serif',
+                                color: '#1E64C8',
+                                borderRadius: '6px',
+                                padding: '10px'
+                            }}/>
                         </Box>
-                        <Box sx={{ marginTop: '16px' }}>
-                            <label htmlFor="choice" style={{ fontSize: '32px', fontFamily: 'Arial, sans-serif', color: 'darkblue', marginBottom: '-10px', display: 'block' }}>Access</label><br />
-                            <select id="choice" name="choice" style={{ fontSize: '20px', fontFamily: 'Arial, sans-serif', borderRadius: '6px', padding: '5px' }}>
+                        <Box sx={{marginTop: '16px'}}>
+                            <label htmlFor="choice" style={{
+                                fontSize: '32px',
+                                fontFamily: 'Arial, sans-serif',
+                                color: '#1E64C8',
+                                marginBottom: '-10px',
+                                display: 'block'
+                            }}>Access</label><br/>
+                            <select id="choice" name="choice" style={{
+                                fontSize: '20px',
+                                fontFamily: 'Arial, sans-serif',
+                                borderRadius: '6px',
+                                padding: '5px'
+                            }}>
                                 <option value="option1">Request access</option>
                                 <option value="option2">Option 2</option>
                                 <option value="option3">Option 3</option>
                             </select>
                         </Box>
-                        <Box sx={{ marginTop: '16px', position: 'absolute' }}>
-                            <button type="submit" style={{backgroundColor: 'blue', color: 'white', padding: '8px 16px', border: 'none', borderRadius: '6px', cursor: 'pointer', fontFamily: 'Arial, sans-serif', fontSize: '16px', marginTop: '80px' }}>{translations.t("Save course")}</button>
+                        <Box sx={{marginTop: '16px', position: 'absolute'}}>
+                            <button type="submit" style={{
+                                backgroundColor: '#1E64C8',
+                                color: 'white',
+                                padding: '8px 16px',
+                                border: 'none',
+                                borderRadius: '6px',
+                                cursor: 'pointer',
+                                fontFamily: 'Arial, sans-serif',
+                                fontSize: '16px',
+                                marginTop: '80px'
+                            }}>{translations.t("Save course")}</button>
                         </Box>
                     </form>
                 </Box>
