ed4c5464c6c5168fc4abb8ff931e6425b66f00a2
robinpdev <robinparet22@gmail.com>
2024-04-10 20:50:23 +0200

    add custom 404 page

---
+++ frontend/app/[...not-found]/page.tsx
@@ -0,0 +1,30 @@
+import NavBar from "@app/[locale]/components/NavBar";
+import Typography from "@mui/material/Typography";
+import Box from "@mui/material/Box";
+import initTranslations from "@app/i18n";
+import TranslationsProvider from "@app/[locale]/components/TranslationsProvider";
+import CreateCourseForm from "@app/[locale]/components/CreateCourseForm";
+
+const backend_url = process.env['NEXT_PUBLIC_BACKEND_URL'];
+
+
+async function CourseCreatePage({ params: { locale } }: { params: { locale: any } }) {
+    const {t, resources} = await initTranslations(locale, ["common"])
+
+    return (
+        <TranslationsProvider
+            resources={resources}
+            locale={locale}
+            namespaces={["common"]}
+        >
+            <NavBar />
+            <Box sx={{ marginTop: '64px' }}>
+                <Typography variant="h3">
+                    {t("page_not_found")}
+                </Typography>
+            </Box>
+        </TranslationsProvider>
+    );
+}
+
+export default CourseCreatePage;
