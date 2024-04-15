aa3af75a808a53fc4b2a936e7a170be7edf4f3a0
Reinhard <reinharddp@gmail.com>
2024-04-15 21:48:46 +0200

    lint fix

--- backend/pigeonhole/apps/users/migrations/0001_initial.py
+++ backend/pigeonhole/apps/users/migrations/0001_initial.py
@@ -19,22 +19,33 @@ class Migration(migrations.Migration):
         migrations.CreateModel(
             name='User',
             fields=[
-                ('password', models.CharField(max_length=128, verbose_name='password')),
-                ('last_login', models.DateTimeField(blank=True, null=True, verbose_name='last login')),
+                ('password', models.CharField(max_length=128,
+                                              verbose_name='password')),
+                ('last_login', models.DateTimeField(blank=True, null=True,
+                                                    verbose_name='last login')),
                 ('is_superuser', models.BooleanField(default=False,
-                                                     help_text='Designates that this user has all pe'
-                                                               'rmissions without explicitly assigning them.',
+                                                     help_text='Designates that th'
+                                                               'is user has all pe'
+                                                               'rmissions without ex'
+                                                               'plicitly assigning them.',
                                                      verbose_name='superuser status')),
-                ('username', models.CharField(error_messages={'unique': 'A user with that username already exists.'},
-                                              help_text='Required. 150 characters or fewer. Letters, digits and @/./+/-/_ only.',
-                                              max_length=150, unique=True, validators=[django.contrib.auth.validators.UnicodeUsernameValidator()],
+                ('username', models.CharField(error_messages={'unique': 'A user with '
+                                                                        'that username already exists.'},
+                                              help_text='Required. 150 characters or f'
+                                                        'ewer. Letters, digits and @/./+/-/_ only.',
+                                              max_length=150, unique=True,
+                                              validators=[django.contrib.auth.validators.
+                                              UnicodeUsernameValidator()],
                                               verbose_name='username')),
                 ('is_staff', models.BooleanField(default=False,
-                                                 help_text='Designates whether the user can log into this admin site.',
+                                                 help_text='Designates whether the us'
+                                                           'er can log into this admin site.',
                                                  verbose_name='staff status')),
                 ('is_active', models.BooleanField(default=True,
-                                                  help_text='Designates whether this user should be treate'
-                                                            'd as active. Unselect this instead of deleting accounts.',
+                                                  help_text='Designates whether '
+                                                            'this user should be treate'
+                                                            'd as active. Unselect '
+                                                            'this instead of deleting accounts.',
                                                   verbose_name='active')),
                 ('date_joined', models.DateTimeField(default=django.utils.timezone.now,
                                                      verbose_name='date joined')),
@@ -42,18 +53,24 @@ class Migration(migrations.Migration):
                 ('email', models.EmailField(max_length=254, unique=True)),
                 ('first_name', models.CharField(max_length=30)),
                 ('last_name', models.CharField(max_length=150)),
-                ('role', models.IntegerField(choices=[(1, 'Admin'), (2, 'Teacher'), (3, 'Student')], default=3)),
+                ('role', models.IntegerField(choices=[(1, 'Admin'), (2, 'Teacher'),
+                                                      (3, 'Student')], default=3)),
                 ('course', models.ManyToManyField(blank=True, to='courses.course')),
                 ('groups', models.ManyToManyField(blank=True,
-                                                  help_text='The groups this user belongs to. A user w'
-                                                            'ill get all permissions granted to each of their groups.',
+                                                  help_text='The groups this us'
+                                                            'er belongs to. A user w'
+                                                            'ill get all permissions'
+                                                            ' granted to each of their groups.',
                                                   related_name='user_set',
                                                   related_query_name='user', to='auth.group',
                                                   verbose_name='groups')),
                 ('user_permissions', models.ManyToManyField(blank=True,
-                                                            help_text='Specific permissions for this user.',
-                                                            related_name='user_set', related_query_name='user',
-                                                            to='auth.permission', verbose_name='user permissions')),
+                                                            help_text='Specific permis'
+                                                                      'sions for this user.',
+                                                            related_name='user_set',
+                                                            related_query_name='user',
+                                                            to='auth.permission',
+                                                            verbose_name='user permissions')),
             ],
             options={
                 'verbose_name': 'user',
