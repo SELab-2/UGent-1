180ed6fa532aaa38e7195464ec51f8b94d11563f
Pieter-Jan De Smijter <pieterjan.desmijter@gmail.com>
2024-04-15 10:42:11 +0200

    view fix

--- backend/pigeonhole/apps/courses/views.py
+++ backend/pigeonhole/apps/courses/views.py
@@ -7,6 +7,8 @@ from backend.pigeonhole.apps.courses.models import CourseSerializer
 from backend.pigeonhole.apps.groups.models import Group
 from backend.pigeonhole.apps.projects.models import Project
 from backend.pigeonhole.apps.projects.models import ProjectSerializer
+from backend.pigeonhole.apps.users.models import User
+from backend.pigeonhole.apps.users.models import UserSerializer
 from .models import Course
 from .permissions import CourseUserPermissions
 
@@ -21,7 +23,7 @@ class CourseViewSet(viewsets.ModelViewSet):
         user = self.request.user
         user.course.add(course)
 
-    @action(detail=True, methods=['post'])
+    @action(detail=True, methods=["post"])
     def join_course(self, request, *args, **kwargs):
         course = self.get_object()
         user = request.user
@@ -31,25 +33,36 @@ class CourseViewSet(viewsets.ModelViewSet):
                 user.course.add(course)
                 return Response(status=status.HTTP_200_OK)
             else:
-                return Response({'message': 'Invite token required.'}, status=status.HTTP_403_FORBIDDEN)
+                return Response(
+                    {"message": "Invite token required."},
+                    status=status.HTTP_403_FORBIDDEN,
+                )
         else:
             user.course.add(course)
             return Response(status=status.HTTP_200_OK)
 
-    @action(detail=True, methods=['post'], url_path='join_course_with_token/(?P<invite_token>[^/.]+)')
+    @action(
+        detail=True,
+        methods=["post"],
+        url_path="join_course_with_token/(?P<invite_token>[^/.]+)",
+    )
     def join_course_with_token(self, request, *args, **kwargs):
         course = self.get_object()
         user = request.user
-        invite_token = kwargs.get('invite_token')
+        invite_token = kwargs.get("invite_token")
 
         if invite_token == course.invite_token:
             user.course.add(course)
-            return Response({'message': 'Successfully joined the course with invite token.'},
-                            status=status.HTTP_200_OK)
+            return Response(
+                {"message": "Successfully joined the course with invite token."},
+                status=status.HTTP_200_OK,
+            )
         else:
-            return Response({'error': 'Invalid invite token.'}, status=status.HTTP_400_BAD_REQUEST)
+            return Response(
+                {"error": "Invalid invite token."}, status=status.HTTP_400_BAD_REQUEST
+            )
 
-    @action(detail=True, methods=['post'])
+    @action(detail=True, methods=["post"])
     def leave_course(self, request, *args, **kwargs):
         course = self.get_object()
         user = request.user
@@ -63,18 +76,29 @@ class CourseViewSet(viewsets.ModelViewSet):
                         group.save()
             user.course.remove(course)
         else:
-            return Response({'message': 'User is not in course'}, status=status.HTTP_400_BAD_REQUEST)
+            return Response(
+                {"message": "User is not in course"}, status=status.HTTP_400_BAD_REQUEST
+            )
         return Response(status=status.HTTP_200_OK)
 
-    @action(detail=False, methods=['GET'])
+    @action(detail=False, methods=["GET"])
     def get_selected_courses(self, request, *args, **kwargs):
         user = request.user
         courses = user.course.all()
         serializer = CourseSerializer(courses, many=True)
         return Response(serializer.data, status=status.HTTP_200_OK)
 
-    @action(detail=True, methods=['GET'])
+    @action(detail=True, methods=["GET"])
+    def get_users(self, request, *args, **kwargs):
+        course = self.get_object()
+        users = User.objects.filter(course=course)
+        res = [user for user in users if course in user.course.all()]
+        return Response(UserSerializer(res, many=True).data, status=status.HTTP_200_OK)
+
+    @action(detail=True, methods=["GET"])
     def get_projects(self, request, *args, **kwargs):
         course = self.get_object()
         projects = Project.objects.filter(course_id=course)
-        return Response(ProjectSerializer(projects, many=True).data, status=status.HTTP_200_OK)
+        return Response(
+            ProjectSerializer(projects, many=True).data, status=status.HTTP_200_OK
+        )
