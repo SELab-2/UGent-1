9bd1fc52d0ca50091d345e869c8e1d872d52f305
Pieter-Jan De Smijter <pieterjan.desmijter@gmail.com>
2024-04-17 12:20:57 +0200

    fix lint

--- backend/pigeonhole/apps/courses/views.py
+++ backend/pigeonhole/apps/courses/views.py
@@ -8,7 +8,7 @@ from backend.pigeonhole.apps.courses.models import CourseSerializer
 from backend.pigeonhole.apps.groups.models import Group
 from backend.pigeonhole.apps.projects.models import Project
 from backend.pigeonhole.apps.projects.models import ProjectSerializer
-from backend.pigeonhole.apps.users.models import User, UserSerializer, Roles
+from backend.pigeonhole.apps.users.models import User, UserSerializer
 from .models import Course
 from .permissions import CourseUserPermissions
 
