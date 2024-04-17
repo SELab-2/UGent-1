02b52c9f0a8b564d8b4eea88ff7b6e590902ff2b
Axel Lorreyne <axellorreyne2@gmail.com>
2024-04-17 23:07:11 +0200

    fix submission listview

--- frontend/app/[locale]/components/ProjectSubmissionsList.tsx
+++ frontend/app/[locale]/components/ProjectSubmissionsList.tsx
@@ -5,6 +5,7 @@ import React from "react";
 
 const ProjectSubmissionsList = ({project_id}: { project_id: number }) => {
     const headers = ["Group number", "Submission date", "Status"]
+    const sortable = [true, true, false]
 
     return (
         <ListView
@@ -12,6 +13,7 @@ const ProjectSubmissionsList = ({project_id}: { project_id: number }) => {
             headers={headers}
             get={'submissions'}
             get_id={project_id}
+            sortable={sortable}
         />
     )
 }
