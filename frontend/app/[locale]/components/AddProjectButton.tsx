6d33fb1315eff15d03a86ce3cd71472a4f9e574d
PJDeSmijter <pieterjan.desmijter@gmail.com>
2024-04-18 21:44:39 +0200

    Revert "fix student view course page"
    This reverts commit 53886677a1e7088ea471b11e742762f9b17d7550.

--- frontend/app/[locale]/components/AddProjectButton.tsx
+++ frontend/app/[locale]/components/AddProjectButton.tsx
@@ -3,8 +3,6 @@ import {useTranslation} from "react-i18next";
 import {Button, Typography} from "@mui/material";
 import Link from "next/link";
 import {addProject} from "@lib/api";
-import {useState, useEffect} from "react";
-import {getUserData} from "@lib/api";
 
 interface EditCourseButtonProps{
     course_id:number
@@ -12,42 +10,30 @@ interface EditCourseButtonProps{
 
 const AddProjectButton = ({course_id}: EditCourseButtonProps) => {
     const {t} = useTranslation();
-    const [role, setRole] = useState(0);
 
-    useEffect(() => {
-        const fetchData = async () => {
-            const userData = await getUserData();
-            setRole(userData.role);
-        }
-        fetchData();
-    }
-    , []);
-
-    if (role !== 3) {
-        return (
-            <Button
-                variant="contained"
-                color="secondary"
+    return (
+        <Button
+            variant="contained"
+            color="secondary"
+            sx={{
+                margin: '10px'
+            }}
+            onClick={async () => {
+                const project_id = await addProject(course_id);
+                window.location.href = `/project/${project_id}/edit`;
+            }}
+        >
+            <Typography
+                variant="subtitle1"
                 sx={{
-                    margin: '10px'
-                }}
-                onClick={async () => {
-                    const project_id = await addProject(course_id);
-                    window.location.href = `/project/${project_id}/edit`;
+                    color: 'secondary.contrastText',
+                    display: 'inline-block',
+                    whiteSpace: 'nowrap',
                 }}
             >
-                <Typography
-                    variant="subtitle1"
-                    sx={{
-                        color: 'secondary.contrastText',
-                        display: 'inline-block',
-                        whiteSpace: 'nowrap',
-                    }}
-                >
-                    {t("add_project")}
-                </Typography>
-            </Button>
-            )
-    } 
+                {t("add_project")}
+            </Typography>
+        </Button>
+    )
 }
 export default AddProjectButton
