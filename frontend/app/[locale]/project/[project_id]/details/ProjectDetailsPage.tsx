b15d413fb8f25efd89c5f5bcd8eb14b1c4e25090
robinpdev <robinparet22@gmail.com>
2024-04-18 16:36:41 +0200

    submission tweaks

--- frontend/app/[locale]/project/[project_id]/details/ProjectDetailsPage.tsx
+++ frontend/app/[locale]/project/[project_id]/details/ProjectDetailsPage.tsx
@@ -21,6 +21,7 @@ import CourseReturnButton from "@app/[locale]/project/[project_id]/details/Cours
 import ProjectEditButton from "@app/[locale]/project/[project_id]/details/ProjectEditButton";
 import ProjectGroupButton from "@app/[locale]/project/[project_id]/details/ProjectGroupButton";
 import GroupSubmissionList from "@app/[locale]/components/GroupSubmissionList";
+import AddButton from "@app/[locale]/components/AddButton";
 
 const backend_url = process.env['NEXT_PUBLIC_BACKEND_URL'];
 
@@ -83,6 +84,7 @@ const ProjectDetailsPage: React.FC<ProjectDetailsPageProps> = ({locale, project_
                                     project_id={project_id}
                                 />
                             </Grid>
+                            <AddButton translationkey="submit_project" href={`/project/${project_id}/submit`}></AddButton>
                             <Typography variant={"h4"}>
                                 {t("assignment")}
                             </Typography>
