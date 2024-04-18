91cb5b0c24ab0c59439eedcebcc85f9345442db5
gilles-arnout <gilles.arnout@gmail.com>
2024-04-18 17:25:50 +0200

    final push

--- frontend/app/[locale]/components/ProfileCard.tsx
+++ frontend/app/[locale]/components/ProfileCard.tsx
@@ -1,24 +1,32 @@
 "use client";
-import React, { useEffect, useState } from 'react';
-import { Stack, ThemeProvider } from '@mui/material';
+import React, {useEffect, useState} from 'react';
+import {Stack, ThemeProvider} from '@mui/material';
 import ProfileCardTheme from '../../../styles/theme';
 import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
 import Typography from '@mui/material/Typography';
 import Avatar from '@mui/material/Avatar';
 import Box from '@mui/material/Box';
-import { APIError, getImage, getUserData, UserData } from "@lib/api";
-import { displayRole } from '@lib/utils';
-import { useTranslation } from "react-i18next";
+import {APIError, getImage, getUserData, UserData} from "@lib/api";
+import {displayRole} from '@lib/utils';
+import {useTranslation} from "react-i18next";
 import Button from "@mui/material/Button";
 import EditIcon from "@mui/icons-material/Edit";
 import Link from "next/link";
 
 const ProfileCard = () => {
-    const [user, setUser] = useState<UserData>({id: 0, email: "", first_name: "", last_name: "", course: [], role: 3, picture: ""});
+    const [user, setUser] = useState<UserData>({
+        id: 0,
+        email: "",
+        first_name: "",
+        last_name: "",
+        course: [],
+        role: 3,
+        picture: ""
+    });
     const [error, setError] = useState<APIError | null>(null);
     const [imageSrc, setImageSrc] = useState<string | null>(null);
-    const { t } = useTranslation();
+    const {t} = useTranslation();
 
     useEffect(() => {
         const fetchUser = async () => {
@@ -45,9 +53,9 @@ const ProfileCard = () => {
                     <CardContent>
                         <Stack direction="column" alignItems="center" spacing={2}>
                             <Avatar
-                                sx={{width: 125, height: 125}}
                                 src={imageSrc}
                                 alt="Profile Image"
+                                sx={{width: 140, height: 140, cursor: 'pointer'}}
                             />
                             <Typography variant="h5" component="div">
                                 {user.first_name} {user.last_name}
@@ -64,7 +72,7 @@ const ProfileCard = () => {
                                 <Button
                                     variant="contained"
                                     color="primary"
-                                    startIcon={<EditIcon />}
+                                    startIcon={<EditIcon/>}
                                 >
                                     {t('edit_account')}
                                 </Button>
