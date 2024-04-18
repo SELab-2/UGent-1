b1c2d624029e2c884bc40ca2ca4d2935b9a3a2ca
gilles-arnout <gilles.arnout@gmail.com>
2024-04-18 19:58:56 +0200

    commit before pull

---
+++ frontend/__test__/ProfileEditCard.test.tsx
@@ -0,0 +1,49 @@
+import React from 'react';
+import {fireEvent, screen, render, waitFor} from '@testing-library/react';
+import ProfileEditCard from "@app/[locale]/components/ProfileCard";
+import '@testing-library/jest-dom';
+import * as api from '@lib/api';
+
+jest.mock('react-i18next', () => ({
+    useTranslation: () => ({t: (key: any) => key})
+}));
+
+jest.mock('../lib/api', () => ({
+    getUserData: jest.fn(),
+    getImage: jest.fn(),
+    APIError: jest.fn(),
+}));
+
+describe('ProfileEditCard', () => {
+
+    it('renders user name correctly', async () => {
+        (api.getUserData as jest.Mock).mockResolvedValueOnce({
+            id: 1,
+            email: "test@gmail.com",
+            first_name: "First",
+            last_name: "Last",
+            course: [1],
+            role: 2,
+            picture: "http://localhost:8000/media/profile_pictures/test.png"
+        })
+
+        const {getByText} = render(<ProfileEditCard/>);
+
+        await waitFor(() => {
+            expect(getByText('First Last')).toBeInTheDocument();
+            expect(getByText('test@gmail.com')).toBeInTheDocument();
+        })
+    })
+
+    it('changes page after clicking save_changes button', async () => {
+        const {getByText} = render(<ProfileEditCard/>);
+        const button = screen.getByRole('button');
+
+        delete window.location
+        window.location = {href: ''};
+
+        await fireEvent.click(button);
+
+        expect(window.location.href).toBe('/profile');
+    })
+})
