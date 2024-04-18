53886677a1e7088ea471b11e742762f9b17d7550
Pieter-Jan De Smijter <pieterjan.desmijter@gmail.com>
2024-04-18 21:41:00 +0200

    fix student view course page

--- frontend/app/[locale]/components/AddProjectButton.tsx
+++ frontend/app/[locale]/components/AddProjectButton.tsx
@@ -3,6 +3,8 @@ import {useTranslation} from "react-i18next";
 import {Button, Typography} from "@mui/material";
 import Link from "next/link";
 import {addProject} from "@lib/api";
+import {useState, useEffect} from "react";
+import {getUserData} from "@lib/api";
 
 interface EditCourseButtonProps{
     course_id:number
@@ -10,30 +12,42 @@ interface EditCourseButtonProps{
 
 const AddProjectButton = ({course_id}: EditCourseButtonProps) => {
     const {t} = useTranslation();
+    const [role, setRole] = useState(0);
 
-    return (
-        <Button
-            variant="contained"
-            color="secondary"
-            sx={{
-                margin: '10px'
-            }}
-            onClick={async () => {
-                const project_id = await addProject(course_id);
-                window.location.href = `/project/${project_id}/edit`;
-            }}
-        >
-            <Typography
-                variant="subtitle1"
+    useEffect(() => {
+        const fetchData = async () => {
+            const userData = await getUserData();
+            setRole(userData.role);
+        }
+        fetchData();
+    }
+    , []);
+
+    if (role !== 3) {
+        return (
+            <Button
+                variant="contained"
+                color="secondary"
                 sx={{
-                    color: 'secondary.contrastText',
-                    display: 'inline-block',
-                    whiteSpace: 'nowrap',
+                    margin: '10px'
+                }}
+                onClick={async () => {
+                    const project_id = await addProject(course_id);
+                    window.location.href = `/project/${project_id}/edit`;
                 }}
             >
-                {t("add_project")}
-            </Typography>
-        </Button>
-    )
+                <Typography
+                    variant="subtitle1"
+                    sx={{
+                        color: 'secondary.contrastText',
+                        display: 'inline-block',
+                        whiteSpace: 'nowrap',
+                    }}
+                >
+                    {t("add_project")}
+                </Typography>
+            </Button>
+            )
+    } 
 }
 export default AddProjectButton
