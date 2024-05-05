c7259048106af676ec8cb5eae35f7865a6d8da49
Axel Lorreyne <axellorreyne2@gmail.com>
2024-05-05 04:36:08 +0200

    add evaluation functionality to submission model

--- backend/pigeonhole/apps/submissions/models.py
+++ backend/pigeonhole/apps/submissions/models.py
@@ -1,6 +1,8 @@
 import os
 
 from django.db import models
+from docker import DockerClient
+from docker.errors import ContainerError, APIError
 from rest_framework import serializers
 
 from backend.pigeonhole.apps.groups.models import Group
@@ -8,12 +10,12 @@ from backend.pigeonhole.apps.groups.models import Group
 
 def get_upload_to(self, filename):
     return (
-        "submissions/"
-        + str(self.group_id.group_id)
-        + "/"
-        + str(self.submission_nr)
-        + "/input"
-        + os.path.splitext(filename)[1]
+            "submissions/"
+            + str(self.group_id.group_id)
+            + "/"
+            + str(self.submission_nr)
+            + "/input"
+            + os.path.splitext(filename)[1]
     )
 
 
@@ -31,18 +33,50 @@ class Submissions(models.Model):
     file_urls = models.TextField(null=True)
     timestamp = models.DateTimeField(auto_now_add=True, blank=True)
     draft = models.BooleanField(default=True)
+    eval_result = models.BooleanField(default=False)
     objects = models.Manager()
 
     # submission_nr is automatically assigned and unique per group, and
     # increments
     def save(
-        self, force_insert=False, force_update=False, using=None, update_fields=None
+            self, force_insert=False, force_update=False, using=None, update_fields=None
     ):
         if not self.submission_nr:
             self.submission_nr = (
-                Submissions.objects.filter(group_id=self.group_id).count() + 1
+                    Submissions.objects.filter(group_id=self.group_id).count() + 1
             )
         super(Submissions, self).save(force_insert, force_update, using, update_fields)
+
+        self.eval()
+
+    def eval(self):
+        client = DockerClient(base_url='unix://var/run/docker.sock')
+
+        try:
+            container = client.containers.run(
+                # TODO: replace busybox placeholder image with valid evaluation image
+                image='busybox:latest',
+                name=f'pigeonhole-submission-{self.submission_id}-evaluation',
+                detach=False,
+                volumes={
+                    'submissions': {
+                        'bind': f'/usr/src/submissions/{self.submission_id}',
+                        'mode': 'ro'
+                    }
+                }
+            )
+
+        except ContainerError as _:
+            self.eval_result = False
+
+        except APIError as _:
+            raise IOError('There was an error evaluation the submission')
+
+        client.close()
+
+        # For now, an error thrown by eval() is interpreted as a failed submission and
+        # exit code 0 as a successful submission
+        # The container object returns the container logs and can be analyzed further
 
 
 class SubmissionsSerializer(serializers.ModelSerializer):
