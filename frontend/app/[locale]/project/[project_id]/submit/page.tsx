9534889af7cf8db306afa21d19fe8747ff6f91fd
gilles-arnout <gilles.arnout@gmail.com>
2024-05-06 14:54:42 +0200

    small fixes

--- frontend/app/[locale]/project/[project_id]/submit/page.tsx
+++ frontend/app/[locale]/project/[project_id]/submit/page.tsx
@@ -2,17 +2,13 @@ import React from 'react';
 import initTranslations from "@app/i18n";
 import TranslationsProvider from "@app/[locale]/components/TranslationsProvider";
 import NavBar from "@app/[locale]/components/NavBar";
-import Footer from "@app/[locale]/components/Footer";
-import SubmitDetailsPage from './SubmitDetailsPage';
+import SubmitDetailsPage from '@app/[locale]/components/SubmitDetailsPage';
 const i18nNamespaces = ['common']
 
 export default async function Course({params: {locale, project_id}, searchParams: {token}}:
                                          { params: { locale: any, project_id: string }, searchParams: { token: string } }) {
     const {t, resources} = await initTranslations(locale, i18nNamespaces)
 
-    const project_selected = false
-
-    const desc_mock = "TODO: zet hier indieninstructies van het project, en misschien ook nog groepnummer, ook vorige indieningen een samenvatting ofzo"
     return (
         <TranslationsProvider
             resources={resources}
@@ -21,7 +17,7 @@ export default async function Course({params: {locale, project_id}, searchParams
         >
             <NavBar/>
 
-            <SubmitDetailsPage project_id={project_id}>
+            <SubmitDetailsPage locale={locale} project_id={project_id}>
 
             </SubmitDetailsPage>
 
