81f5a5f470a3f5c8139c04134e51f1a52a8597b1
gilles-arnout <gilles.arnout@gmail.com>
2024-05-06 11:15:31 +0200

    beginning refactor project-submit-page

--- frontend/app/[locale]/project/[project_id]/submit/SubmitDetailsPage.tsx
+++ frontend/app/[locale]/project/[project_id]/submit/SubmitDetailsPage.tsx
@@ -2,9 +2,13 @@
 
 import React, {useEffect, useState} from 'react';
 import {useTranslation} from "react-i18next";
-import {Grid, LinearProgress, ThemeProvider, Typography} from "@mui/material";
+import {Grid, Button, Divider, Card, IconButton, CardContent, LinearProgress, ThemeProvider, Typography} from "@mui/material";
 import {getProject, Project, uploadSubmissionFile} from '@lib/api';
 import baseTheme from "@styles/theme";
+import ProjectReturnButton from "@app/[locale]/components/ProjectReturnButton";
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import ExpandLessIcon from '@mui/icons-material/ExpandLess';
+import PublishIcon from '@mui/icons-material/Publish';
 
 interface SubmitDetailsPageProps {
     project_id: number;
@@ -17,18 +21,8 @@ const SubmitDetailsPage: React.FC<SubmitDetailsPageProps> = ({project_id}) => {
     const [paths, setPaths] = useState<string[]>([]);
     const [submitted, setSubmitted] = useState<string>("no");
     const [loadingProject, setLoadingProject] = useState<boolean>(true);
-
-    function folderAdded(event: any) {
-        let newpaths: string[] = []
-        for (const file of event.target.files) {
-            let text: string = file.webkitRelativePath;
-            if (text.includes("/")) {
-                text = text.substring((text.indexOf("/") ?? 0) + 1, text.length);
-            }
-            newpaths.push(text);
-        }
-        setPaths(newpaths);
-    }
+    const [isExpanded, setIsExpanded] = useState(false);
+    const previewLength = 300;
 
     useEffect(() => {
         const fetchProject = async () => {
@@ -42,83 +36,112 @@ const SubmitDetailsPage: React.FC<SubmitDetailsPageProps> = ({project_id}) => {
         fetchProject().then(() => setLoadingProject(false));
     }, [project_id]);
 
+    function folderAdded(event: any) {
+        let newpaths: string[] = []
+        for (const file of event.target.files) {
+            let text: string = file.webkitRelativePath;
+            if (text.includes("/")) {
+                text = text.substring((text.indexOf("/") ?? 0) + 1, text.length);
+            }
+            newpaths.push(text);
+        }
+        setPaths(newpaths);
+    }
+
     if (loadingProject) {
         return <LinearProgress/>;
     }
 
+    const toggleDescription = () => {
+        setIsExpanded(!isExpanded);
+    }
+
     return (
         <ThemeProvider theme={baseTheme}>
-            <Grid container alignItems="flex-start" justifyContent="flex-start" style={{ minHeight: '100vh', padding: 0 }}>
-                <Grid item xs={12} style={{ position: 'absolute', top: 84, left: 20 }}>
-                    
-                <Typography
-                    variant="h3"
-                    sx={{
-                        fontWeight: 'medium',
-                        marginTop: 2
-                    }}
-                >
-                    {t('submit_project')}: {projectData?.name}
-                </Typography>
-                <Typography variant="h6">
-                    {projectData?.description}
-                </Typography>
-                <Typography
-                    variant="h3"
-                    sx={{
-                        fontWeight: 'medium',
-                        marginTop: 2
-                    }}
-                >
-                    {t('files')}
-                </Typography>
+            <Grid container alignItems="flex-start" justifyContent="flex-start"
+                  style={{minHeight: '100vh', padding: 0}}>
+                <Grid item xs={12} style={{position: 'absolute', top: 84, left: 20}}>
+                    <ProjectReturnButton locale="en" project_id={projectData?.project_id}/>
+                </Grid>
+                <Grid item xs={12} style={{display: 'flex', justifyContent: 'center', paddingTop: 20}}>
+                    <Card raised style={{width: 1000}}>
+                        <CardContent>
+                            <Typography
+                                variant="h3"
+                                sx={{
+                                    fontWeight: 'medium'
+                                }}
+                            >
+                                {t('submit_project')}: {projectData?.name}
+                            </Typography>
+                            <Divider style={{marginBottom: 10, marginTop: 10}}/>
+                            <Typography>
+                                {projectData?.description && projectData?.description.length > previewLength && !isExpanded
+                                    ? `${projectData?.description.substring(0, previewLength)}...`
+                                    : projectData?.description
+                                }
+                            </Typography>
+                            {projectData?.description && projectData?.description.length > previewLength && (
+                                <IconButton
+                                    color="primary"
+                                    onClick={toggleDescription}
+                                    sx={{ flex: '0 0 auto', padding: 0 }}
+                                >
+                                    {isExpanded ? <ExpandLessIcon /> : <ExpandMoreIcon />}
+                                </IconButton>
+                            )}
+                            <Typography
+                                variant="h4"
+                                sx={{
+                                    fontWeight: 'medium',
+                                    marginTop: 2
+                                }}
+                            >
+                                {t('files')}
+                            </Typography>
 
 
-                <form onSubmit={async (e) => {
-                    setSubmitted(await uploadSubmissionFile(e, project_id));
-                }} encType="multipart/form-data">
+                            <form onSubmit={async (e) => {
+                                setSubmitted(await uploadSubmissionFile(e, project_id));
+                            }} encType="multipart/form-data">
 
-                    <input
-                        style={{
-                            width: "300px",
-                            height: "120px",
-                            backgroundColor: "lightgrey",
-                            border: "6px dotted black"
-                        }}
-                        onChange={folderAdded} type="file" id="filepicker" name="fileList" webkitdirectory="true"
-                        multiple/>
+                                <input
+                                    style={{
+                                        width: "300px",
+                                        height: "120px",
+                                        backgroundColor: "lightgrey",
+                                        border: "6px dotted black"
+                                    }}
+                                    onChange={folderAdded} type="file" id="filepicker" name="fileList"
+                                    webkitdirectory="true"
+                                    multiple/>
 
-                    <input type="hidden" name="project_id" value={project_id}/>
+                                <input type="hidden" name="project_id" value={project_id}/>
 
-                    <ul id="listing"></ul>
+                                <ul id="listing"></ul>
 
-                    <ul>
-                        {paths.map(path => (
-                            <li key={path}>{path}</li>
-                        ))}
-                    </ul>
-                    {submitted === "yes" && <Typography variant="h6">{t('submitted')}</Typography>}
-                    {submitted === "error" && <Typography variant="h6">{t('submission_error')}</Typography>}
-                    {submitted !== "yes" &&
-                        <button type='submit'
-                                style={{
-                                    backgroundColor: '#1E64C8',
-                                    color: 'white',
-                                    padding: '8px 16px',
-                                    border: 'none',
-                                    borderRadius: '6px',
-                                    cursor: 'pointer',
-                                    fontFamily: 'Quicksand',
-                                    fontSize: '16px',
-                                    marginTop: '10px',
-                                    marginLeft: '15px'
-                                }}>
-                            <Typography variant="h6">{t("submit")}</Typography>
-                        </button>
-                    }
-                </form>
-
-            </div>
+                                <ul>
+                                    {paths.map(path => (
+                                        <li key={path}>{path}</li>
+                                    ))}
+                                </ul>
+                                {submitted === "yes" && <Typography variant="h6">{t('submitted')}</Typography>}
+                                {submitted === "error" && <Typography variant="h6">{t('submission_error')}</Typography>}
+                                {submitted !== "yes" &&
+                                    <Button
+                                        variant="contained"
+                                        color="primary"
+                                        startIcon={<PublishIcon/>}
+                                        type="submit"
+                                    >
+                                        {t("submit")}
+                                    </Button>
+                                }
+                            </form>
+                        </CardContent>
+                    </Card>
+                </Grid>
+            </Grid>
         </ThemeProvider>
     );
 }
