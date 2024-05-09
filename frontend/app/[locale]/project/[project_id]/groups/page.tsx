741f705ed819b2d0b02eb92a252b246d5254e93b
gilles-arnout <gilles.arnout@gmail.com>
2024-05-09 14:43:00 +0200

    add text to listviews

--- frontend/app/[locale]/project/[project_id]/groups/page.tsx
+++ frontend/app/[locale]/project/[project_id]/groups/page.tsx
@@ -1,9 +1,11 @@
 import initTranslations from "@app/i18n";
 import TranslationsProvider from "@app/[locale]/components/TranslationsProvider";
 import NavBar from "@app/[locale]/components/NavBar";
-import Footer from "@app/[locale]/components/Footer";
 import ListView from '@app/[locale]/components/ListView';
 import BackButton from "@app/[locale]/components/BackButton";
+import ArrowBackIcon from "@mui/icons-material/ArrowBack";
+import Button from "@mui/material/Button";
+import React from "react";
 
 const i18nNamespaces = ['common']
 
@@ -21,11 +23,15 @@ export default async function GroupPage({ params }: { params: { locale: any, pro
             namespaces={i18nNamespaces}
         >
             <NavBar />
-            <div style={{marginTop:60, padding:20}}>
-            <BackButton 
-            destination={`/project/${projectId}`} 
-            text={t('back_to') + ' ' + t('project') + ' ' +  t('page')}
-            />
+            <div style={{marginTop:20, padding:20}}>
+                <Button
+                    variant="outlined"
+                    color="primary"
+                    startIcon={<ArrowBackIcon/>}
+                    href={`/${locale}/project/${projectId}`}
+                >
+                    {t("return_course")}
+                </Button>
                 <ListView
                     admin={true}
                     headers={headers}
@@ -33,6 +39,7 @@ export default async function GroupPage({ params }: { params: { locale: any, pro
                     sortable={[true, false, false]}
                     get_id={projectId}
                     get={'groups'}
+                    search_text={t("group_search")}
                 />
             </div>
         </TranslationsProvider>
