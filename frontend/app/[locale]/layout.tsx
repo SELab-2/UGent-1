44fcb9fef9f0ef3fd0926ca5eeba91daac1efeb6
Pieter-Jan De Smijter <pieterjan.desmijter@gmail.com>
2024-05-19 10:01:37 +0200

    mobile home page merge fix

--- frontend/app/[locale]/layout.tsx
+++ frontend/app/[locale]/layout.tsx
@@ -1,6 +1,5 @@
 import {AppRouterCacheProvider} from '@mui/material-nextjs/v13-appRouter';
 import {ThemeProvider} from '@mui/material/styles';
-import baseTheme from '@styles/theme';
 import {Box} from '@mui/material';
 import baseTheme from '../../styles/theme';
 import React from "react";
