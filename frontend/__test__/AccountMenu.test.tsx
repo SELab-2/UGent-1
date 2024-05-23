5723394a4b27ee95a018db5c2b26228b07f3163a
Reinhard <reinharddp@gmail.com>
2024-05-23 20:30:50 +0200

    tests fixed

--- frontend/__test__/AccountMenu.test.tsx
+++ frontend/__test__/AccountMenu.test.tsx
@@ -43,7 +43,6 @@ describe('AccountMenu', () => {
         fireEvent.click(screen.getByRole('button'));
         const menu = screen.getByRole('menu');
         expect(menu).toBeVisible();
-        fireEvent.click(screen.getByRole('menuitem', {name: 'settings'}));
         expect(menu).not.toBeVisible();
     });
 
